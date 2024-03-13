21b15f57125c0bf42f4d6a00b02b128bed63edc1
linhung0319 <linhung0319@gmail.com>
2024-03-13 10:47:50 +0800

    feat: check which button is pressed

--- game.js
+++ game.js
@@ -2,8 +2,15 @@
 
 buttonColors = ["red", "blue", "green", "yellow"];
 let gamePattern = [];
-
+let userClickedPattern = [];
 nextSequence();
+
+$(".btn").click(function() {
+    let userChosenColor = $(this).attr("id");
+    userClickedPattern.push(userChosenColor);
+    console.log(userClickedPattern);
+})
+
 function nextSequence() {
     let randomNumber = Math.floor(Math.random() * 4);
     let randomChosenColor = buttonColors[randomNumber];
